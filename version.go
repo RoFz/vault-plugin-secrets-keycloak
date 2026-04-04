9f3b6991f13b51058b0e3e28fe70c3e3ac9fb2a4
RoFz <4117458+RoFz@users.noreply.github.com>
2026-04-04 10:52:53 +0100

    fix: use generic updater for version.go with v prefix
    - fix: switch from version-file to extra-files with generic updater
    - fix: restore v prefix in version.go to match Vault conventions
    - fix: add x-release-please-version annotation for generic updater

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package secretsengine
 
 // Version is the current version of the plugin, reported to Vault via
 // framework.Backend.RunningVersion.
-const Version = "v0.0.0"
+const Version = "v0.1.0" // x-release-please-version
